a60b52efcc46d0f7a04e55957fae876dbba1e7d1
topdev0104 <simonweingand73@gmail.com>
2022-03-25 22:34:36 +0900

    fix: the port

--- server.js
+++ server.js
@@ -70,6 +70,6 @@ try {
     });
   });
 } catch (e) {}
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
